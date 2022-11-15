a261b5529cb7b7da71cb65483a9215e6d4f2493f
psa21u347 <stepaha78@gmail.com>
2022-11-15 14:03:24 +0300

    Fix comparator

--- C_Labs/lab_10_01_01/func_tests/scripts/comparator.sh
+++ C_Labs/lab_10_01_01/func_tests/scripts/comparator.sh
@@ -4,16 +4,10 @@
 file_1="$1"
 file_2="$2"
 
-# Регулярное выражение для поиска действительных чисел
-regex="\-?[0-9]+\.?[0-9]*"
-
-# Найденные числа
-first_digits="$(grep -Eo "$regex" "$file_1")"
-second_digits="$(grep -Eo "$regex" "$file_2")"
 
 # Сравнение ответов
-if [ "$first_digits" == "$second_digits" ]; then
-    exit 0
-else
+if [ "$(diff "$file_1" "$file_2")" ]; then
     exit 1
+else
+    exit 0
 fi
